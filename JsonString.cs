d83828f799cc9ef6af1f6e90545a897cf01a9c0b
HDoru <32764088+HDoru@users.noreply.github.com>
2021-03-30 19:11:04 +0300

    Create JsonString.cs

---
+++ JsonString.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Json
+{
+    public static class JsonString
+    {
+        public static bool IsJsonString(string input)
+        {
+            return IsDoubleQuoted(input);
+        }
+
+        public static bool IsDoubleQuoted(string input)
+        {
+            const int atleastchar = 2;
+            return input.Length >= atleastchar && input[0] == '"' && input[^1] == '"';
+        }
+    }
+}
