9ba019cb4e40efed98bfe8427f9ba8b36025c767
HDoru <32764088+HDoru@users.noreply.github.com>
2021-04-18 20:25:43 +0300

    Update JsonNumbers.cs
    am scris prima functie

--- JsonNumbers.cs
+++ JsonNumbers.cs
@@ -1,1 +1,13 @@
 
+using System;
+
+namespace Json
+{
+    public static class JsonNumber
+    {
+        public static bool IsJsonNumber(string input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
