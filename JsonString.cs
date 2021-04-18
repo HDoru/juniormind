25854bb1050a8eccada030dc4b6eb19cea7ba729
HDoru <32764088+HDoru@users.noreply.github.com>
2021-04-18 20:14:38 +0300

    Update JsonString.cs
    am schimbat funcia de  ContainsControlCharacters pentru ca nu putea valida testul cu specialcarachters
    ->am adaugat functia CheckValidEscapeCharacters care imi valideaza unicode

--- JsonString.cs
+++ JsonString.cs
@@ -4,37 +4,29 @@ namespace Json
 {
     public static class JsonString
     {
+        const int QuotationNumber = 2;
+        const int UnicodeNumbers = 4;
+
         public static bool IsJsonString(string input)
         {
-            return HasContent(input) && IsDoubleQuoted(input) && !ContainsControlCharacters(input);
+             return HasContent(input)
+                && IsDoubleQuoted(input)
+                && !ContainsControlCharacters(input)
+                && !ContainsUnrecognizedEscapeCharacters(input);
         }
 
         static bool ContainsControlCharacters(string input)
         {
-            const char N = '\\';
-            if (input.Contains(N))
+            foreach (char character in input)
             {
-                return CountControlCharacters(input) > 1;
-            }
-
-            return !input.Contains(N);
-        }
-
-        static int CountControlCharacters(string input)
-        {
-            int count = 0;
-            foreach (char c in input)
-            {
-                if (char.IsControl(c))
+                if (char.IsControl(character))
                 {
-                    count++;
+                    return true;
                 }
             }
 
-            return count;
+            return input[input.Length - QuotationNumber] == '\\' || input[input.Length - QuotationNumber] == 'u';
         }
-
-
 
         static bool IsDoubleQuoted(string input)
         {
@@ -45,6 +37,62 @@ namespace Json
         static bool HasContent(string input)
         {
             return !string.IsNullOrEmpty(input);
+        }
+
+        static bool ContainsUnrecognizedEscapeCharacters(string input)
+        {
+            bool valid = true;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '\\')
+                {
+                    valid = CheckValidEscapeCharacters(input, ref i);
+                }
+
+                if (!valid)
+                {
+                    return !valid;
+                }
+            }
+
+            return !valid;
+        }
+
+        static bool IsValidHex(char input)
+        {
+            return input >= 'a' && input <= 'f' || input >= '0' && input <= '9';
+        }
+
+        static bool CheckValidHexNumbers(string input)
+        {
+            for (int i = 1; i < input.Length; i++)
+            {
+                if (!IsValidHex(input.ToLower()[i]))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        static bool CheckValidEscapeCharacters(string input, ref int i)
+        {
+            const string validEscapeCharacters = @"bntrf\/\""";
+            i++;
+
+            if (input[i] == 'u' && input.Length - QuotationNumber - i >= UnicodeNumbers && CheckValidHexNumbers(input.Substring(i, UnicodeNumbers)))
+            {
+                i += UnicodeNumbers;
+                return true;
+            }
+            else if (validEscapeCharacters.Contains(input.Substring(i, 1)))
+            {
+                i++;
+                return true;
+            }
+
+            return false;
         }
     }
 }
