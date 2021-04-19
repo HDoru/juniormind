ab8fb28b484cdb45c18aaadf3af5e085cd6596d4
HDoru <32764088+HDoru@users.noreply.github.com>
2021-04-19 20:02:50 +0300

    Update JsonNumbers.cs

--- JsonNumbers.cs
+++ JsonNumbers.cs
@@ -1,13 +1,23 @@
-
-using System;
-
 namespace Json
 {
     public static class JsonNumber
     {
         public static bool IsJsonNumber(string input)
         {
-            throw new NotImplementedException();
+            return IsDigitsOnly(input);
+        }
+
+        public static bool IsDigitsOnly(string input)
+        {
+            foreach (char c in input)
+            {
+                if (c < '0' || c > '9')
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
