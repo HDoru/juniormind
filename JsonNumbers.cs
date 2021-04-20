a30b856eafd9d3b15de8e5cd4729a7f0674db966
HDoru <32764088+HDoru@users.noreply.github.com>
2021-04-20 22:37:45 +0300

    Update JsonNumbers.cs
    Am adaugat funcia de CanBeFractional. Cu ajutorul acestia putem sa vedem daca un numar este rational. Apoi am inceput sa fac primele teste pentru numere care sunt ridicate la putere cu ajutorul litere e/E

--- JsonNumbers.cs
+++ JsonNumbers.cs
@@ -13,13 +13,48 @@ namespace Json
         {
             foreach (char c in input)
             {
-                if (c < '0' || c > '9')
+                if (c < '0' || c < '9' || c == 'e' || c == 'E')
                 {
-                    return false;
+                    return true;
                 }
             }
 
-            return true;
+            return false;
+        }
+
+        static int CountSpecificCharacter(string input, char specificcharacter)
+        {
+            int count = 0;
+            foreach (char c in input)
+            {
+                if (c == specificcharacter)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        static void BreakStringInTwo(string input, ref string firstpartofinput, ref string secondpartofinput)
+        {
+            int indexofpoint = input.IndexOf('.');
+            firstpartofinput = input.Substring(0, indexofpoint);
+            secondpartofinput = input.Substring(indexofpoint + 1, input.Length - 1 - indexofpoint);
+        }
+
+        static bool CanBeFractional(string input)
+            {
+            string firststring = string.Empty;
+            string secondstring = string.Empty;
+
+            if (CountSpecificCharacter(input, '.') != 1)
+            {
+                return false;
+            }
+
+            BreakStringInTwo(input, ref firststring, ref secondstring);
+            return IsDigitsOnly(firststring) && IsDigitsOnly(secondstring) && HasContent(firststring) && HasContent(secondstring);
         }
 
         static bool IsNegativNumber(string input)
@@ -28,6 +63,11 @@ namespace Json
             if (input[0] == '-')
             {
                 numberwithoutminus = input.Remove(0, 1);
+            }
+
+            if (numberwithoutminus.Contains('.'))
+            {
+               return CanBeFractional(numberwithoutminus);
             }
 
             return IsDigitsOnly(numberwithoutminus);
@@ -40,7 +80,7 @@ namespace Json
 
         static bool NumberDoesNotStartWithZero(string input)
         {
-            return !input.StartsWith('0') || input.Length <= 1;
+            return !input.StartsWith('0') || input.Length <= 1 || input.Contains('.');
         }
     }
 }
