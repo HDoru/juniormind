1bce9411fb9e3905243e5ca32609aa919eacb12a
HDoru <32764088+HDoru@users.noreply.github.com>
2021-04-21 21:37:56 +0300

    Update JsonNumbers.cs

--- JsonNumbers.cs
+++ JsonNumbers.cs
@@ -6,14 +6,14 @@ namespace Json
         {
             return HasContent(input)
                 && NumberDoesNotStartWithZero(input)
-                && IsNegativNumber(input);
+                && IsValidNumber(input);
         }
 
         public static bool IsDigitsOnly(string input)
         {
             foreach (char c in input)
             {
-                if (c < '0' || c < '9' || c == 'e' || c == 'E')
+                if (c < '0' || c < '9')
                 {
                     return true;
                 }
@@ -57,20 +57,64 @@ namespace Json
             return IsDigitsOnly(firststring) && IsDigitsOnly(secondstring) && HasContent(firststring) && HasContent(secondstring);
         }
 
-        static bool IsNegativNumber(string input)
+        static bool IsValidNumber(string input)
+        {
+            if (input[0] == '-')
+            {
+                input = input.Remove(0, 1);
+            }
+
+            if (input.Contains('.'))
+            {
+                return false;
+            }
+
+            if (input.Contains('e') || input.Contains('E'))
+            {
+                return Exponential(input);
+            }
+
+            return IsDigitsOnly(input);
+        }
+
+        static bool Exponential(string input)
+        {
+            string exponentnumber = string.Empty;
+            if (CountSpecificCharacter(input.ToLower(), 'e') > 1)
+            {
+                return false;
+            }
+
+            ExponentNumber(input, ref exponentnumber);
+
+            return VerifyExponentNumber(exponentnumber);
+        }
+
+        static bool VerifyExponentNumber(string input)
         {
             string numberwithoutminus = input;
-            if (input[0] == '-')
+            if (!HasContent(input))
+            {
+                return false;
+            }
+
+            if (input[0] == '-' || input[0] == '+')
             {
                 numberwithoutminus = input.Remove(0, 1);
             }
 
             if (numberwithoutminus.Contains('.'))
             {
-               return CanBeFractional(numberwithoutminus);
+                return CanBeFractional(numberwithoutminus);
             }
 
-            return IsDigitsOnly(numberwithoutminus);
+            return IsDigitsOnly(numberwithoutminus) && HasContent(numberwithoutminus);
+        }
+
+        static void ExponentNumber(string input, ref string exponent)
+        {
+            int indexofpoint = input.ToLower().IndexOf('e');
+            exponent = input.Substring(indexofpoint + 1, input.Length - 1 - indexofpoint);
         }
 
         static bool HasContent(string input)
