f7220c4b10ea4dcbcfee4e18a370a2985421afe1
HDoru <32764088+HDoru@users.noreply.github.com>
2021-05-05 21:07:22 +0300

    added SoccerTeamsRanking

---
+++ SoccerTeamsRanking/SoccerTeamsRanking/SoccerTeam.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoccerTeamsRanking
+{
+    class SoccerTeam
+    {
+        private int id;
+        private string name;
+
+
+        public SoccerTeam(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+
+    }
+}
---
+++ SoccerTeamsRanking/SoccerTeamsRankingFacts/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace SoccerTeamsRankingFacts
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
