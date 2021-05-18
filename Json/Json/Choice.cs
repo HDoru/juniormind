fa92e6256f5d4e515bce93e54e449b44644032c3
HDoru <32764088+HDoru@users.noreply.github.com>
2021-05-18 21:59:57 +0300

    added json

---
+++ Json/Json/Choice.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Json
+{
+
+    class Choice : IPattern
+    {
+        private IPattern[] patterns;
+
+        public Choice(params IPattern[] patterns)
+        {
+            this.patterns = patterns;
+           
+        }
+
+
+        public bool Match(string text)
+        {
+           
+        }
+    }
+
+
+
+}
