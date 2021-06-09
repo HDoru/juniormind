5fb0cb4c15ccc11b5c8597319ca74096983a2b23
HDoru <32764088+HDoru@users.noreply.github.com>
2021-06-09 21:04:14 +0300

    added Range

--- JsonPartTwo/Json/Range.cs
+++ JsonPartTwo/Json/Range.cs
@@ -4,7 +4,25 @@ using System.Text;
 
 namespace Json
 {
-    class Range
+    class Range :IPattern
     {
+        readonly char start;
+    readonly char end;
+
+    public Range(char start, char end)
+    {
+        this.start = start;
+        this.end = end;
+    }
+
+        public IMatch Match(string text)
+        {
+            if(string.IsNullOrEmpty(text) || text[0] < start || text[0] > end)
+            {
+                return new Match(text, false);
+            }
+
+            return new Match(text[1..], true);
+        }
     }
 }
