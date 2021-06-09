3ae24963ec53f2f74509818d0209b3be6c758e5e
HDoru <32764088+HDoru@users.noreply.github.com>
2021-06-09 20:56:37 +0300

    Added Choice

--- JsonPartTwo/Json/Choice.cs
+++ JsonPartTwo/Json/Choice.cs
@@ -6,5 +6,25 @@ namespace Json
 {
     class Choice
     {
+        private readonly IPattern[] patterns;
+
+        public Choice(params IPattern[] patterns)
+        {
+            this.patterns = patterns;
+        }
+
+        public IMatch Match(string text)
+        {
+            foreach (var pattern in patterns)
+            {
+                var match = pattern.Match(text);
+                if (match.Success())
+                {
+                    return match;
+                }
+            }
+
+            return new Match(text, false);
+        }
     }
 }
