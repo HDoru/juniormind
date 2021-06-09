d7d7f38eadaafffc8e7673743cce3d0bce536426
HDoru <32764088+HDoru@users.noreply.github.com>
2021-06-09 22:26:28 +0300

    added text

---
+++ JsonPartTwo/Json/Text.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Json
+{
+    public class Text : IPattern
+    {
+        string prefix;
+
+        public Text(string prefix)
+        {
+            this.prefix = prefix;
+        }
+
+        public IMatch Match(string text)
+        {
+            if(string.IsNullOrEmpty(text) || !text.StartsWith(prefix))
+            {
+                return new Match(text, false);
+            }
+            return new Match(text[1..], true);
+        }
+
+    }
+}
