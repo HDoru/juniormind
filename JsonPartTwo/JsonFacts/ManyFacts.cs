f1b9bf98d190e665de067f5cd11905311ae71b56
HDoru <32764088+HDoru@users.noreply.github.com>
2021-06-17 13:30:32 +0300

    added ManyFacts

---
+++ JsonPartTwo/JsonFacts/ManyFacts.cs
@@ -0,0 +1,64 @@
+﻿using Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace JsonFacts
+{
+    public class ManyFacts
+    {
+        [Fact]
+
+        public void TextMatch()
+        {
+            var a = new Many(new Character('a'));
+            Assert.True(a.Match("abcd").Success());
+            Assert.Equal("bcd", a.Match("abcd").RemainingText());
+        }
+
+        [Fact]
+        public void MuplipleTextMatch()
+        {
+            var a = new Many(new Character('a'));
+            Assert.True(a.Match("aaaabcd").Success());
+            Assert.Equal("bcd", a.Match("aaaabcd").RemainingText());
+        }
+
+        [Fact]
+        public void TextNotMatch()
+        {
+            var a = new Many(new Character('a'));
+            Assert.True(a.Match("bcd").Success());
+            Assert.Equal("bcd", a.Match("bcd").RemainingText());
+        }
+
+        [Fact]
+        public void TextEmpty()
+        {
+            var a = new Many(new Character('a'));
+            Assert.True(a.Match("").Success());
+            Assert.Equal("", a.Match("").RemainingText());
+        }
+
+        [Fact]
+        public void TextNull()
+        {
+            var a = new Many(new Character('a'));
+            Assert.True(a.Match(null).Success());
+            Assert.Null(a.Match(null).RemainingText()); ;
+        }
+
+        [Fact]
+
+        public void IsInRange()
+        {
+            var digits = new Many(new Json.Range('0', '9'));
+            Assert.True(digits.Match("12345ab123").Success());
+            Assert.Equal("ab123", digits.Match("12345ab123").RemainingText());
+
+        }
+
+
+    }
+}
