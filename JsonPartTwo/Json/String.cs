af908e792cb3441c9dc7d597d71d5830c48d1cce
HDoru <32764088+HDoru@users.noreply.github.com>
2021-06-23 21:11:23 +0300

    first test

---
+++ JsonPartTwo/Json/String.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Json
+{
+   public class String:IPattern
+    {
+        readonly IPattern pattern;
+
+        public String()
+        {
+            var hex = new Choice(
+
+                new Range('a', 'f'));
+              
+
+            var quote = new Character('"');
+
+          
+            ;
+            var characters = new Optional(
+                new Sequence(new Many(hex)));
+
+            this.pattern = new Sequence(quote, characters, quote);
+
+
+        }
+
+        public IMatch Match(string text)
+        {
+            return pattern.Match(text);
+        }
+    }
+}
---
+++ JsonPartTwo/JsonFacts/StringFacts.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Json;
+
+namespace JsonFacts
+{
+    public class StringFacts
+    {
+        [Fact]
+        public void IsWrappedInDoubleQuotes()
+        {
+            var word = new Json.String(); 
+            Assert.True(word.Match(Quoted("abc")).Success());
+            Assert.Equal("", word.Match(Quoted("abc")).RemainingText());
+        }
+
+        /*[Fact]
+        public void AlwaysStartsWithQuotes()
+        {
+            Assert.False(IsJsonString("abc\""));
+        }
+
+        [Fact]
+        public void AlwaysEndsWithQuotes()
+        {
+            Assert.False(IsJsonString("\"abc"));
+        }
+
+        [Fact]
+        public void IsNotNull()
+        {
+            Assert.False(IsJsonString(null));
+        }
+
+        [Fact]
+        public void IsNotAnEmptyString()
+        {
+            Assert.False(IsJsonString(string.Empty));
+        }
+
+        [Fact]
+        public void IsAnEmptyDoubleQuotedString()
+        {
+            Assert.True(IsJsonString(Quoted(string.Empty)));
+        }
+
+        [Fact]
+        public void HasStartAndEndQuotes()
+        {
+            Assert.False(IsJsonString("\""));
+        }
+
+
+        [Fact]
+        public void DoesNotContainControlCharacters()
+        {
+            Assert.False(IsJsonString(Quoted("a\nb\rc")));
+        }
+
+        [Fact]
+        public void CanContainLargeUnicodeCharacters()
+        {
+            Assert.True(IsJsonString(Quoted("⛅⚾")));
+        }
+
+        [Fact]
+        public void CanContainEscapedQuotationMark()
+        {
+            Assert.True(IsJsonString(Quoted(@"\""a\"" b")));
+        }
+
+        [Fact]
+        public void CanContainEscapedReverseSolidus()
+        {
+            Assert.True(IsJsonString(Quoted(@"a \\ b")));
+        }
+
+        [Fact]
+        public void CanContainEscapedSolidus()
+        {
+            Assert.True(IsJsonString(Quoted(@"a \/ b")));
+        }
+
+        [Fact]
+        public void CanContainEscapedBackspace()
+        {
+            Assert.True(IsJsonString(Quoted(@"a \b b")));
+        }
+
+        [Fact]
+        public void CanContainEscapedFormFeed()
+        {
+            Assert.True(IsJsonString(Quoted(@"a \f b")));
+        }
+
+        [Fact]
+        public void CanContainEscapedLineFeed()
+        {
+            Assert.True(IsJsonString(Quoted(@"a \n b")));
+        }
+
+        [Fact]
+        public void CanContainEscapedCarrigeReturn()
+        {
+            Assert.True(IsJsonString(Quoted(@"a \r b")));
+        }
+
+        [Fact]
+        public void CanContainEscapedHorizontalTab()
+        {
+            Assert.True(IsJsonString(Quoted(@"a \t b")));
+        }
+
+        [Fact]
+        public void CanContainEscapedUnicodeCharacters()
+        {
+            Assert.True(IsJsonString(Quoted(@"a \u26Be b")));
+        }
+
+        [Fact]
+        public void DoesNotContainUnrecognizedExcapceCharacters()
+        {
+            Assert.False(IsJsonString(Quoted(@"a\x")));
+        }
+
+        [Fact]
+        public void DoesNotEndWithReverseSolidus()
+        {
+            Assert.False(IsJsonString(Quoted(@"a\")));
+        }
+
+        [Fact]
+        public void DoesNotEndWithAnUnfinishedHexNumber()
+        {
+            Assert.False(IsJsonString(Quoted(@"a\u")));
+            Assert.False(IsJsonString(Quoted(@"a\u123")));
+        }
+*/
+ 
+        public static string Quoted(string text)
+            => $"\"{text}\"";
+    }
+       
+}
