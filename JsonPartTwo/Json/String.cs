223c47cdeda8828ee54d40a22ffc2226d20c9c09
HDoru <32764088+HDoru@users.noreply.github.com>
2021-07-13 18:56:42 +0300

    no message

--- JsonPartTwo/Json/String.cs
+++ JsonPartTwo/Json/String.cs
@@ -30,7 +30,7 @@ namespace Json
                 new Character('\\'),
                 escape));
                                        ;
-            var characters = new Many(new OneOrMore(character));
+            var characters = new Optional(new Many(character));
 
             this.pattern = new Sequence(quote, characters, quote);
         }
